71c52859f9ba5e50e56205c1602d7fc9a1af330f
David Muhr <dmuhr@hotmail.com>
2018-02-28 19:53:37 +0100

    feat(Pandoc): Table of content support

--- app/Lib/Pandoc.hs
+++ app/Lib/Pandoc.hs
@@ -20,18 +20,32 @@ pandocWriterOptions  = defaultHakyllWriterOptions {
         writerHTMLMathMethod = KaTeX ""
       }
 
+-- Add TOC to WriterOptions
+pandocWriterOptionsWithTOC :: WriterOptions
+pandocWriterOptionsWithTOC = pandocWriterOptions {
+        writerTableOfContents = True,
+        writerTOCDepth = 2,
+        writerTemplate = Just "\n<div class=\"toc\">\n$toc$\n</div>\n$body$"
+      }
+
 -- Conditionally compile with or without citations
-compilePandoc :: Item CSL -> [Item Biblio] -> Compiler (Item String)
-compilePandoc _ [] =
-  pandocCompilerWith pandocReaderOptions pandocWriterOptions
-compilePandoc csl (bib:_) =
-  fmap (writePandocWith pandocWriterOptions)
-      (getResourceBody >>= readPandocBiblio pandocReaderOptions csl bib)
+writePandoc' :: ReaderOptions -> WriterOptions -> Item CSL -> [Item Biblio] -> Compiler (Item String)
+writePandoc' ropt wopt _ [] =
+  pandocCompilerWith ropt wopt
+writePandoc' ropt wopt csl (bib:_) =
+  fmap (writePandocWith wopt)
+      (getResourceBody >>= readPandocBiblio ropt csl bib)
 
 pandocCompiler' :: Compiler (Item String)
 pandocCompiler' = do
   -- get the path to the current folder
   currentRoute <- (takeDirectory . toFilePath) <$> getUnderlying
   bib <- loadAll (fromGlob (currentRoute ++ "/*.bib"))
-  csl <- load $ fromFilePath "assets/csl/ieee-with-url.csl"
-  compilePandoc csl bib
+  csl <- load $ fromFilePath "assets/csl/ieee.csl"
+  id <- getUnderlying
+  toc <- getMetadataField id "toc"
+  let ropt = pandocReaderOptions
+  let wopt = case toc of
+        Just _  -> pandocWriterOptionsWithTOC
+        Nothing -> pandocWriterOptions
+  writePandoc' ropt wopt csl bib
