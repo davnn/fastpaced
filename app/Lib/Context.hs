42883e439142f0d26c31a149c989b52500d2309e
David Muhr <dmuhr@hotmail.com>
2018-03-18 21:01:19 +0100

    feat(context): Add meta description

--- app/Lib/Context.hs
+++ app/Lib/Context.hs
@@ -5,16 +5,18 @@ import System.FilePath (dropFileName)
 
 site = "https://curious.observer"
 title = "Curious Observer | Data. Science. Code. Design. Thoughts."
+description = "Curious Observer is a blog about science and technology. This is a place to learn about interesting ideas explained from first principles."
 
 -- Based on `urlField`
 shortUrlField :: String -> Context a
 shortUrlField key = field key $
     fmap (maybe mempty (toUrl . dropFileName)) . getRoute . itemIdentifier
 
--- Field that should be in all contexts
+-- Field that should exist in all contexts (for all pages)
 extendedDefaultContext :: Context String
 extendedDefaultContext =
   constField "site" site `mappend`
+  constField "description" description `mappend`
   defaultContext
 
 indexCtx :: [Item String] -> Context String
